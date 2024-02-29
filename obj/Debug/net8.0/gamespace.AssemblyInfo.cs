9d7e3fa6769fab9345039c6b4c739fa7f8b4f373
Pwsjas <pwsjas433@gmail.com>
2024-02-29 00:07:10 -0500

    implement GenreContainer and GenreCard components

--- obj/Debug/net8.0/gamespace.AssemblyInfo.cs
+++ obj/Debug/net8.0/gamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce3b61d9e30b35b2fa8566f019711c17c120d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65252ec150cb5b5279bf88beed596e8b6015ebd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
