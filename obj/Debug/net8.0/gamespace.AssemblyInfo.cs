279dd707faa8dcb7c7b7c605cb07829307a92e2b
Pwsjas <pwsjas433@gmail.com>
2024-03-05 01:44:38 -0500

    add dynamic loading to GamePage, fix various null reference possibilities

--- obj/Debug/net8.0/gamespace.AssemblyInfo.cs
+++ obj/Debug/net8.0/gamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05fb6a7d924b5c9fda949b6cb8d983930e41b3b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a694028c9d343a610ceffeb78e36c09c7c308e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
