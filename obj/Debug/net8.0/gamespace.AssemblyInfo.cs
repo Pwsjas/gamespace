fba42031a296cbccb2d7b70e184cbedcb1259d8e
Pwsjas <pwsjas433@gmail.com>
2024-03-09 17:12:06 -0500

    Create GenreSelection and PlatformSelection components, add basic communication and data storage functionality

--- obj/Debug/net8.0/gamespace.AssemblyInfo.cs
+++ obj/Debug/net8.0/gamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed0ceb45328ad882cf101879c15389d53328948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e61fce06c81b2ca5b7402342775cf73435dd13a")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
