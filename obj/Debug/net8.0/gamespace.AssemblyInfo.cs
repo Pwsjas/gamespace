0a1f5ca5cdd2bac7623f6b30ee5b31a98e113372
Pwsjas <pwsjas433@gmail.com>
2024-03-17 13:27:11 -0400

    Move Websites to GameTitleCard and add SVG images

--- obj/Debug/net8.0/gamespace.AssemblyInfo.cs
+++ obj/Debug/net8.0/gamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9827b3e12bfb936d5c9ed4d4e206ba157e1cbac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8952c30e0c5ac82aacfb98fdd843e6230238e9a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
