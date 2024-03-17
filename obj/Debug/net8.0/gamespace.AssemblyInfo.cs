5a08e8f73c29f2824986be6d18bc9517aa482a04
Pwsjas <pwsjas433@gmail.com>
2024-03-17 14:54:15 -0400

    redesign color scheme, fix majority of .NET async warnings

--- obj/Debug/net8.0/gamespace.AssemblyInfo.cs
+++ obj/Debug/net8.0/gamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8952c30e0c5ac82aacfb98fdd843e6230238e9a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1f5ca5cdd2bac7623f6b30ee5b31a98e113372")]
 [assembly: System.Reflection.AssemblyProductAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
